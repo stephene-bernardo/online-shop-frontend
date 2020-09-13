4d45571a03b6edcb069b9e3b3d9b55b31149b981
Karl Bernardo <stephene.bernardo@gmail.com>
2020-09-14 00:54:26 +0800

    replace prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  onlineShopBackendURL: "https://karl-online-shop-backend.herokuapp.com"
+  onlineShopBackendURL: "https://stephene-online-shop-backend.herokuapp.com"
 };
